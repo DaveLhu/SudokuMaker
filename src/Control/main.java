a1138aca0a7e925ced820494107bf7cb4781f754
kixey <kixey@LAPTOP-5NP5FB5I>
2020-10-13 17:45:08 +0100

    Fonctionnement ok ?

---
+++ src/Control/main.java
@@ -0,0 +1,10 @@
+package Control;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("does it works ?");
+
+	}
+
+}
---
+++ src/Grile/Grille.java
@@ -0,0 +1,5 @@
+package Grile;
+
+public class Grille {
+
+}
